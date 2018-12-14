3124e70ef48a9eb5b0940a29933b91c58a1d36db
Zanis Ali <zanisali999@gmail.com>
2018-12-14 22:05:22 +0100

    final except food details page

---
+++ app/src/main/java/startup/softflix/com/startup/Food.kt
@@ -0,0 +1,18 @@
+package startup.softflix.com.startup
+
+/**
+ * Created by Zanis on 12/14/2018.
+ */
+
+class Food{
+    var name:String?=null
+    var des:String?=null
+    var image:Int?=null
+
+    constructor(name:String,des:String,image:Int)
+    {
+        this.name=name;
+        this.des=des
+        this.image=image
+    }
+}
